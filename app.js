82bb8d0dd54e71c614af10ba19ec0c331db312d3
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-10-15 11:21:17 +0200

    6

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 
 const port = 3000
 
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
+
+app.post('/', (req, res, next) => {
+    console.log(req.body);
+  })
+  
 
 
 app.use((req, res, next) => {
