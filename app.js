f301f56f0ba0ddacfcedb50b51739bdd904e4bae
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-10-15 09:57:36 +0200

    3

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require("express");
 const app = express();
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
 app.listen(port, () => {
