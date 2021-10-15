b6afaff8a5bf227e0ad79d4175a9e4b085710408
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-10-15 10:52:54 +0200

    5

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
