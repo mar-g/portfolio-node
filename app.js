59bba945b77b8b6ad8fcac887f68fbf289896e44
Marcin Gorniakowski <marcin@gorniakowski.pl>
2021-10-15 10:21:02 +0200

    4

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
